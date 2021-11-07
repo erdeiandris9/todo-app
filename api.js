07b1f1705fbfadc6b028d5f3faf9720d9c90687d
ErdeiAndras <erdeiandris9@gmail.com>
2021-11-07 19:27:14 +0100

    base url upgrade

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
 const httpClient = axios.create({
-	baseURL: "http://localhost:8080",
+  baseURL: "https://ea-todo-app.herokuapp.com",
 });
 
 export default {
-	getTodos() {
-		return httpClient.get("/todos");
-	},
-	updateTodos(todos) {
-		return httpClient.post("/todos", todos);
-	},
+  getTodos() {
+    return httpClient.get("/todos");
+  },
+  updateTodos(todos) {
+    return httpClient.post("/todos", todos);
+  },
 };
