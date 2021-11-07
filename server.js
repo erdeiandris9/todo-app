e898048c45c853723ade79ef013f6abfa0e26238
ErdeiAndras <erdeiandris9@gmail.com>
2021-11-07 19:21:41 +0100

    port update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const todos = require("./todos.json")
+const todos = require("./todos.json");
 const express = require("express");
 const fs = require("fs");
 const app = express();
@@ -6,16 +6,15 @@ app.use(express.static(__dirname));
 app.use(express.json());
 
 app.get("/todos", (req, res) => {
-    res.json(todos)
-    console.log(todos);
-    res.end();
-})
+  res.json(todos);
+  console.log(todos);
+  res.end();
+});
 app.post("/todos", (req, res) => {
-    fs.writeFile("todos.json", JSON.stringify(req.body, null, "\t"), (err) => {
-        if (err) throw err;
-    });
-    res.end();
-})
-const PORT = 8080;
+  fs.writeFile("todos.json", JSON.stringify(req.body, null, "\t"), (err) => {
+    if (err) throw err;
+  });
+  res.end();
+});
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
-
